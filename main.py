6158b8b4497498dd416f1baa51f10ede3e3c3737
cr4zyhog <huiojiu@gmail.com>
2025-04-09 17:59:27 +0300

    weffwerwwrerewewrewrwerwerrwerw

--- main.py
+++ main.py
@@ -1,25 +1,68 @@
-import moderngl_window as mglw
+from math import pi, sin, cos
+
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from direct.actor.Actor import Actor
+from direct.interval.IntervalGlobal import Sequence
+from panda3d.core import Point3
 
 
-class App(mglw.WindowConfig):
-    window_size = 1600, 900
-    resource_dir = 'programs'
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.quad = mglw.geometry.quad_fs()
-        self.program = self.load_program(vertex_shader='vertex.glsl', fragment_shader='fragment.glsl')
-        # uniforms
-        self.program['u_resolution'] = self.window_size
+        # Disable the camera trackball controls.
+        self.disableMouse()
 
-    def render(self, time, frame_time):
-        self.ctx.clear()
-        self.program['u_time'] = time
-        self.quad.render(self.program)
+        # Load the environment model.
+        self.scene = self.loader.loadModel("models/environment")
+        # Reparent the model to render.
+        self.scene.reparentTo(self.render)
+        # Apply scale and position transforms on the model.
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
 
-    def mouse_position_event(self, x, y, dx, dy):
-        self.program['u_mouse'] = (x, y)
+        # Add the spinCameraTask procedure to the task manager.
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
+        # Load and transform the panda actor.
+        self.pandaActor = Actor("models/panda-model",
+                                {"walk": "models/panda-walk4"})
+        self.pandaActor.setScale(0.005, 0.005, 0.005)
+        self.pandaActor.reparentTo(self.render)
+        # Loop its animation.
+        self.pandaActor.loop("walk")
+
+        # Create the four lerp intervals needed for the panda to
+        # walk back and forth.
+        posInterval1 = self.pandaActor.posInterval(13,
+                                                   Point3(0, -10, 0),
+                                                   startPos=Point3(0, 10, 0))
+        posInterval2 = self.pandaActor.posInterval(13,
+                                                   Point3(0, 10, 0),
+                                                   startPos=Point3(0, -10, 0))
+        hprInterval1 = self.pandaActor.hprInterval(3,
+                                                   Point3(180, 0, 0),
+                                                   startHpr=Point3(0, 0, 0))
+        hprInterval2 = self.pandaActor.hprInterval(3,
+                                                   Point3(0, 0, 0),
+                                                   startHpr=Point3(180, 0, 0))
+
+        # Create and play the sequence that coordinates the intervals.
+        self.pandaPace = Sequence(posInterval1, hprInterval1,
+                                  posInterval2, hprInterval2,
+                                  name="pandaPace")
+        self.pandaPace.loop()
+
+    # Define a procedure to move the camera.
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 6.0
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
 
 
 if __name__ == '__main__':
-    mglw.run_window_config(App)
+    app = MyApp()
+    app.run()
